9e51685d20e98b3fd6af5e8ce7d3fceb4bad4de9
trongnk1402 <kt140295@gmail.com>
2019-06-04 21:57:33 +0700

    push 4/6

--- dist/xaydung/public/js/app.js
+++ dist/xaydung/public/js/app.js
@@ -93,7 +93,77 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-
+$(document).ready(function () {
+  var navbar = $('#navbar');
+  var toggle = $('[data-menu]');
+  var close = $('.fa-times');
+  toggle.on('click', function (e) {
+    e.preventDefault();
+    navbar.toggleClass('active');
+  });
+  close.on('click', function (e) {
+    e.preventDefault();
+    navbar.toggleClass('active');
+  });
+  var swiper = new Swiper('.swiper-product', {
+    slidesPerView: 5,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    breakpoints: {
+      1025: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+        simulateTouch: true,
+        autoplay: {
+          delay: 5000
+        }
+      },
+      769: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+        simulateTouch: true,
+        autoplay: {
+          delay: 5000
+        }
+      },
+      440: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        simulateTouch: true,
+        autoplay: {
+          delay: 5000
+        }
+      }
+    }
+  });
+  var swiper = new Swiper('.swiper-work', {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    breakpoints: {
+      769: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+        autoplay: {
+          delay: 5000
+        }
+      },
+      440: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        autoplay: {
+          delay: 5000
+        }
+      }
+    }
+  });
+});
 
 /***/ }),
 
