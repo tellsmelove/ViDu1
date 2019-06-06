32d5800002747e583b39107667f22230dd4e62d4
trongnk1402 <kt140295@gmail.com>
2019-06-06 14:39:27 +0700

    create tmdt

---
+++ dist/tmdt/README.md
@@ -0,0 +1,25 @@
+# Template Name
+|**Author**| **Version** |
+|-|-|
+|*Author Name*| *1.0.0* |
+
+## List Page
+| Name Page | File Page | Status |
+|-|-|-|
+| Trang Chủ | index.html | [x] |
+| Giới Thiệu | about.html | [x] |
+| Giỏ Hàng | cart.html | [x] |
+| Liên Hệ | contact.html | [x] |
+| Chi Tiết Sản Phẩm | detailproduct.html | [x] |
+| Quản Lý Sản Phẩm | order.html | [x] |
+| Thanh Toán | payment.html | [x] |
+| Sản Phẩm | product.html | [x] |
+| Tin Tức | news.html | [x] |
+| Bài Viết | newpost.html | [x] |
+| Đăng Nhập | login.html | [x] |
+| Đăng Ký | registe.html | [x] |
+| Quản Lý Tài Khoản | manage.html | [x] |
+| Chỉnh Sữa Thông Tin | editlocal.html | [x] |
+
+## Screenshot
+![](./screenshot.png)
---
+++ src/tmdt/js/app.js
@@ -0,0 +1,78 @@
+$(document).ready(() => {
+    var navbar = $('#navbar');
+    var toggle = $('[data-menu]');
+    var close = $('.close-menu');
+    toggle.on('click', function (e) {
+        e.preventDefault()
+        $(navbar).toggleClass('active');
+    });
+    close.on('click', function (e) {
+        e.preventDefault()
+        $(navbar).toggleClass('active');
+    });
+    var galleryThumbs = new Swiper('.gallery-thumbs', {
+        spaceBetween: 10,
+        slidesPerView: 4,
+        freeMode: true,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+    });
+    var galleryTop = new Swiper('.gallery-top', {
+        spaceBetween: 10,
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        thumbs: {
+            swiper: galleryThumbs
+        }
+    });
+})
+
+var swiper = new Swiper('.swiper-main', {
+    loop: true,
+    height: 280,
+    autoplay: {
+        delay: 5000,
+    },
+});
+
+$(function () {
+
+    $('.spinner .btn:first-of-type').on('click', function () {
+        var btn = $(this);
+        var input = btn.closest('.spinner').find('input');
+        if (input.attr('max') == undefined || parseInt(input.val()) < parseInt(input.attr('max'))) {
+            input.val(parseInt(input.val(), 10) + 1);
+        } else {
+            btn.next("disabled", true);
+        }
+    });
+    $('.spinner .btn:last-of-type').on('click', function () {
+        var btn = $(this);
+        var input = btn.closest('.spinner').find('input');
+        if (input.attr('min') == undefined || parseInt(input.val()) > parseInt(input.attr('min'))) {
+            input.val(parseInt(input.val(), 10) - 1);
+        } else {
+            btn.prev("disabled", true);
+        }
+    });
+})
+$(document).on('click', '.number-spinner button', function (e) {
+    e.preventDefault()
+    var btn = $(this),
+        oldValue = btn.closest('.number-spinner').find('input').val().trim(),
+        newVal = 0;
+
+    if (btn.attr('data-dir') == 'up') {
+        newVal = parseInt(oldValue) + 1;
+    } else {
+        if (oldValue > 1) {
+            newVal = parseInt(oldValue) - 1;
+        } else {
+            newVal = 1;
+        }
+    }
+    btn.closest('.number-spinner').find('input').val(newVal);
+});
